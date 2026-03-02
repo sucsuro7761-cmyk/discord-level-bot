a779333ecf0c4fb3578c29ed853e1a4ec3dfc1b7
sucsuro7761-cmyk <sucsuro7761@gmail.com>
2026-03-02 23:00:05 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from datetime import datetime, timezone
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 intents.voice_states = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
